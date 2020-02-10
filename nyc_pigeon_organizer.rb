a98d2a57614a6e150c2432ec9034d4660a2de744
Kristine Du <kristine.a.du@gmail.com>
2020-02-10 18:57:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pidgeon_list = {}
+  data.find do |
+  # input HoHoA - [key attribute][key attr_values][value names]
+  # output HoHoA - [key names][key attribute][value attr_values]
+  
+  colors = pigeon_data[:color].keys # .map {|sym| puts sym} # list of :color values
+  colors.# loop through each color key and return name if 
 end
+
+# turn name values into keys
+# turn attr_values keys into strings, values
+# attribute keys remain keys, each name has all attribute keys
+# data.find name in pidgon_list hash, if does not exist, add to hash
