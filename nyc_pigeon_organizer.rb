1dcc6c2c24cbae5d3bc355055ab73f6689724ee6
Kristine Du <kristine.a.du@gmail.com>
2020-02-10 20:28:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,7 +29,8 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name]
           pigeon_list[name] = {}
         end
-        
+        if pigeon_list[name][]
+          
         
       
       end
